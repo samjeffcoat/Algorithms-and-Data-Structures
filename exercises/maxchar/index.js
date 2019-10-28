f1d121c328a9efb1c2bbd58b3d1470c1850ece67
Sam <Jeffcoat>
2019-10-28 10:38:57 -0500

    chunked problem completed and tests pass

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,14 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 // take our string and convert to an object, and key it up
+//assign our charMap to an empty object
+// set initial max to 0
+//maxChar is set to empty string
+
+// for loop to set up an object with keys, and assign those values to each letter or charachter
+
+// The for...of statement creates a loop iterating over iterable objects, 
+//The only difference is over what they iterate: for..in iterates over all enumerable property keys of an object. for..of iterates over the values of an iterable object. Examples of iterable objects are arrays, strings, and NodeLists.
 
 
 
