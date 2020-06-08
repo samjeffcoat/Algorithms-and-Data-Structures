9810ce9fae50f59c6c8261c34643e4e6aaba9ea8
Sam Jeffcoat <samjeffcoat@gmail.com>
2020-06-08 13:58:58 -0500

    linked list practice

---
+++ exercises/linkedLists/index.js
@@ -0,0 +1,45 @@
+// --- Directions
+// Implement classes Node and Linked Lists
+// See 'directions' document
+
+class Node {
+  constructor(value, next = null) {
+    //nothing special about calling it with data
+    this.value = value;
+    this.next = next;
+  }
+}
+
+class LinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
+  }
+  isEmpty() {
+    return this.length === 0;
+  }
+  // just adding a new node to our linked list
+  insertFirst(value) {
+    const newNode = new Node(value);
+
+    if (this.isEmpty()) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      this.tail.next = newNode;
+      this.tail = newNode;
+    }
+  }
+  size() {
+    let counter = 0;
+    let node = this.head;
+    while (node) {
+      counter++;
+      node = node.next;
+    }
+    return counter;
+  }
+}
+
+module.exports = { Node, LinkedList };
